00cbed307344d5b6a7cecbf59c2cd7b139c723e6
Gabriel Pozza <gabriel.pozza@outlook.com>
2025-08-11 17:15:50 +0000

    Unidade deslocamnto a direita

---
+++ srl.sv
@@ -0,0 +1,32 @@
+module srl #(
+  parameter N = 16
+) (
+  input   logic [$clog2(N)-1:0]  amt,   // Quantidade de deslocamento (log2(N) bits)
+  input   logic [N-1:0] dataIn,         // Dado de entrada por tamanho N do hardware
+  output  logic [N-1:0] dataOut         // Dado de saída por tamanho N do hardware
+);
+  localparam K = $clog2(N);
+  wire [N-1:0] s[K-1:0];
+
+  mux2_1 #(.N(N)) mux0(.d0(dataIn), .d1({1'b0, dataIn[N-1:1]}), .sel(amt[0]), .y(s[0]));
+
+  genvar i;
+  generate
+    for (i = 0;i < K-1; i++) begin: shifters
+      localparam SHIFT = 2 << i;
+      mux2_1 #(.N(N)) mux(.d0(s[i]), .d1({{SHIFT{1'b0}}, s[i][N-1:SHIFT]}), .sel(amt[i+1]), .y(s[i+1]));
+    end
+  endgenerate
+  assign dataOut = s[K-1];
+endmodule
+
+module mux2_1 #(
+  parameter N = 4
+)(
+  input  logic [N-1:0] d0, d1,
+  input  logic sel,
+  output logic [N-1:0] y);
+
+  assign y = sel ? d1:d0;
+
+endmodule
--- testbench/tb_rippleSubAdder.sv
+++ testbench/tb_rippleSubAdder.sv
@@ -7,46 +7,54 @@ module tb_rippleSubAdder();
     rippleSubAdder DUT(A, B, D, S, Cout);
 
     initial begin
-        // Teste 1: 1+0
+        // Teste 1: 0+1
         A = 4'b0001; B = 4'b0000; D = 0; #10;
         assert (S === 4'b0001) else $error("Teste 1 Failed.");
-        $display("Test 1: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 2: 1-0
+        $display("Test 1: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 2: 0-1
         A = 4'b0001; B = 4'b0000; D = 1; #10;
         assert (S === 4'b1111) else $error("Teste 2 Failed.");
-        $display("Test 2: %b - %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 3: 0+1
+        $display("Test 2: %b - %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 3: 1+0
         A = 4'b0000; B = 4'b0001; D = 0; #10;
         assert (S === 4'b0001) else $error("Teste 3 Failed.");
-        $display("Test 3: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 4: 0-1
+        $display("Test 3: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 4: 1-0
         A = 4'b0000; B = 4'b0001; D = 1; #10;
-        assert (S === 4'b1111) else $error("Teste 4 Failed.");
-        $display("Test 4: %b - %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 5: 15+0
+        assert (S === 4'b0001) else $error("Teste 4 Failed.");
+        $display("Test 4: %b - %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 5: 0+15
         A = 4'b1111; B = 4'b0000; D = 0; #10;
         assert (S === 4'b1111) else $error("Teste 5 Failed.");
-        $display("Test 5: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 6: 15+1
+        $display("Test 5: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 6: 1+15
         A = 4'b1111; B = 4'b0001; D = 0; #10;
         assert (S === 4'b0000) else $error("Teste 6 Failed.");
-        $display("Test 6: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 7: 15+2
+        $display("Test 6: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 7: 2+15
         A = 4'b1111; B = 4'b0010; D = 0; #10;
         assert (S === 4'b0001) else $error("Teste 7 Failed.");
-        $display("Test 7: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 8: 10+2
+        $display("Test 7: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 8: 2+10
         A = 4'b1010; B = 4'b0010; D = 0; #10;
-        assert (S === 4'b0001) else $error("Teste 8 Failed.");
-        $display("Test 8: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 9: 15-7
+        assert (S === 4'b1100) else $error("Teste 8 Failed.");
+        $display("Test 8: %b + %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 9: 7-15
         A = 4'b1111; B = 4'b0111; D = 1; #10;
-        assert (S === 4'b0001) else $error("Teste 9 Failed.");
-        $display("Test 9: %b + %b = %b / Cout= %b", A, B, S, Cout);
-        // Teste 10: 0-15
+        assert (S === 4'b1000) else $error("Teste 9 Failed.");
+        $display("Test 9: %b - %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 10: 15-0
         A = 4'b0000; B = 4'b1111; D = 1; #10;
-        assert (S === 4'b0001) else $error("Teste 10 Failed.");
-        $display("Test 10: %b + %b = %b / Cout= %b", A, B, S, Cout);
+        assert (S === 4'b1111) else $error("Teste 10 Failed.");
+        $display("Test 10: %b - %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 11: 0-15
+        A = 4'b1111; B = 4'b0000; D = 1; #10;
+        assert (S === 4'b0001) else $error("Teste 11 Failed.");
+        $display("Test 11: %b - %b = %b / Cout= %b", B, A, S, Cout);
+        // Teste 12: 1-1
+        A = 4'b0001; B = 4'b0001; D = 1; #10;
+        assert (S === 4'b0000) else $error("Teste 12 Failed.");
+        $display("Test 12: %b - %b = %b / Cout= %b", B, A, S, Cout);
         
         $display("All testes got completed.");
     end
