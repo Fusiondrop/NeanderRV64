81d90b90b78662769564d03b0bfa1b20c733491a
Gabriel Pozza <gabriel.pozza@outlook.com>
2025-08-12 17:13:14 +0000

    Unidade de deslocamento à esquerda

--- sll.sv
+++ sll.sv
@@ -1,4 +1,4 @@
-module srl #(
+module sll #(
   parameter N = 16
 ) (
   input   logic [$clog2(N)-1:0]  amt,   // Quantidade de deslocamento (log2(N) bits)
@@ -8,13 +8,13 @@ module srl #(
   localparam K = $clog2(N);
   wire [N-1:0] s[K-1:0];
 
-  mux2_1 #(.N(N)) mux0(.d0(dataIn), .d1({1'b0, dataIn[N-1:1]}), .sel(amt[0]), .y(s[0]));
+  mux2_1 #(.N(N)) mux0(.d0(dataIn), .d1({dataIn[N-2:0], 1'b0}), .sel(amt[0]), .y(s[0]));
 
   genvar i;
   generate
     for (i = 0;i < K-1; i++) begin: shifters
       localparam SHIFT = 2 << i;
-      mux2_1 #(.N(N)) mux(.d0(s[i]), .d1({{SHIFT{1'b0}}, s[i][N-1:SHIFT]}), .sel(amt[i+1]), .y(s[i+1]));
+      mux2_1 #(.N(N)) mux(.d0(s[i]), .d1({s[i][(N-1)-SHIFT:0], {SHIFT{1'b0}}}), .sel(amt[i+1]), .y(s[i+1]));
     end
   endgenerate
   assign dataOut = s[K-1];
---
+++ testbench/tb_sll.sv
@@ -0,0 +1,14 @@
+module tb_sll();
+    parameter N = 16;
+
+    logic [N-1:0] dataIn, DataOut;
+    logic [$clog2(N)-1:0] amount;
+
+
+    sll DUT(amount, dataIn, DataOut);
+    initial begin
+        dataIn = 16'b0000_0000_0001_0000; amount = 4'b0100; #10;
+        $display("Test 1: %b << %b by %d", dataIn, DataOut, amount);
+
+    end
+endmodule
